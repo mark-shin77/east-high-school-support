fd5d23b8f34c3eb5ce5342e2ad6b56a8707126f8
Max Wilets <mwilets@gmail.com>
2019-01-15 13:46:08 -0700

    form start

---
+++ client/src/components/loginForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {Link} from 'react-router-dom';
+//login mechanics
+const LoginForm = ({
+    onSubmit,
+    onChange,
+    errors,
+    successMessage,
+    user,
+    toggleAuthenticateStatus
+})=>(
+    <div className="container">
+      <form action="/" onSubmit={onSubmit}>
+        <h2 className="login">Login</h2>
+        {successMessage && <p className="success-message">{successMessage}</p>}
+        {errors.summary && <p className="error-message">{errors.summary}</p>}
+        
+        <div className="field-line">
+          
+        </div>
+      </form>
+    </div>
+)
