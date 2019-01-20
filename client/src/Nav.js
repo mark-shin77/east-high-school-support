ddfe647a899edd4ac3b16556840a3f9e85c270cd
Mark <contact.mshin@gmail.com>
2019-01-20 15:04:44 -0700

    front end styling

---
+++ client/src/Nav.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./App.css";
+import "./assets/css/main.css";
+import "./assets/css/font-awesome.min.css";
+
+class Nav extends Component {
+  render() {
+    return (
+      
+   
+	<nav id="nav">
+      <ul className="container">
+        <li><a href="#top">Top</a></li>
+        <li><a href="#work">Work</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+
+
+   
+
+    );
+  }
+}
+
+export default Nav;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "./assets/css/main.css";
+import "./assets/css/font-awesome.min.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
