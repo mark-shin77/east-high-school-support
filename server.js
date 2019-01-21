999c84ee0df78424d0bdf9ad4fc9ea9b73c0561a
Max Wilets <mwilets@gmail.com>
2019-01-20 18:11:15 -0700

    server

--- server.js
+++ server.js
@@ -21,18 +21,43 @@ const app = express();
     //use passport middleware
     app.use(passport.initialize());
     //Use passport login strategy
-    const localLoginStrategy = require("./server/passport/local-login")
+    const localLoginStrategy = require("./client/server/passport/localStrategy")
     passport.use("local-login", localLoginStrategy)
     //authorization middleware
-    const authCheckMiddleware= require("'./server.middleware/auth-check")
+    const authCheckMiddleware= require("'.server.middleware/auth-check")
     app.use("/api", authCheckMiddleware)
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+
 // Make public a static folder
 // app.use(express.static('client/build'));
+app.use(passport.initialize())
+app.use(passport.session()) // will call the deserializeUser
 
+// ===== testing middleware =====
+// app.use(function(req, res, next) {
+// 	console.log('===== passport user =======')
+// 	console.log(req.session)
+// 	console.log(req.user)
+// 	console.log('===== END =======')
+// 	next()
+// })
+// testing
+// app.get(
+// 	'/auth/google/callback',
+// 	(req, res, next) => {
+// 		console.log(`req.user: ${req.user}`)
+// 		console.log('======= /auth/google/callback was called! =====')
+// 		next()
+// 	},
+// 	passport.authenticate('google', { failureRedirect: '/login' }),
+// 	(req, res) => {
+// 		res.redirect('/')
+// 	}
+// )
 // app.get("*", (req, res) => {
 //     res.sendFile(path.resolve( __dirname, 'client', 'build', 'index.html'));
 // })
