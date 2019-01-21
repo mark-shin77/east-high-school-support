f597e6eb759ccc0bde1e6032ae281d42f90e3d9c
Max Wilets <mwilets@gmail.com>
2019-01-21 14:49:43 -0700

    server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const passport = require('./client/server/passport')
-
+const User = require("./client/server/db/models/user")
 // Initialize Express
 const app = express();
 
@@ -46,56 +46,7 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 //login check
-app.get("login/user", (req,res,next) =>{
-    console.log(req.user)
-    if(req.user){
-        return res.json({user: req.user})
-    }
-    else{
-        return res.json({user: null})
-    }
-})
-app.post("/login", (req,res,next)=>{
-    console.log(req.body)
-    next()
-},
-passport.authenticate('local'), (req, res)=>{
-    const user= JSON.parse(JSON.stringify(req.user))
-    const cleanUser = Object.assign({}, user)
-    if(cleanUser.local){
-        console.log(`Deleting ${cleanUser.local.password}`)
-        delete cleanUser.loca.password
-    }
-    res.json({user: cleanUser})
-})
 
-app.post("/logout", (req,res)=>{
-    if(req.user){
-        req.session.destroy()
-        res.clearCookie('connect.sid')
-        return res.json({msg: 'logging you out'})
-    } else{
-        return res.json({msg: 'no user to log out'})
-    }
-})
-app.post("/signup", (req,res)=>{
-    const {username, password} = req.body
-    User.findOne({'local.username': username},(err, userMatch)=>{
-        if(userMatch){
-            return res.json({
-                error: `Sorry, already a user with the same username ${username}`
-            })
-        }
-        const newUser = new User({
-            'local.username': username,
-            'local.password': password
-        })
-        newUser.save((err,savedUser)=>{
-            if (err) return res.json(err)
-            return res.json(savedUser)
-        })
-    })
-})
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
