8cd7b3591647b3fc8f029e807865271daa7c9fa2
The Lizard <suzpaylor@gmail.com>
2019-01-22 17:52:42 -0700

    pics

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './assets/css/main.css'
 import "./assets/css/font-awesome.min.css";
