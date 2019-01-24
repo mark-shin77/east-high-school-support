1f95536558f9f67a22c29f5707c0f3962dfce7c2
Mark <contact.mshin@gmail.com>
2019-01-23 18:02:01 -0700

    volunteer section

--- client/src/components/AvailableTimeSlots.js
+++ client/src/components/AvailableTimeSlots.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Moment from 'react-moment';
+import 'moment-timezone';
+import CheckAvailability from './test';
 
 const AvailableTimeSlots = (props) => {
+
     return (
         <table>
             <thead>
                 <tr>
+                    <th>Event</th>
+                    <th>Location</th>
                     <th>Date</th>
                     <th>Time</th>
                     <th>Available Slot</th>
@@ -15,11 +20,14 @@ const AvailableTimeSlots = (props) => {
             {props.availableTimeSlots.map(item => {
                 return (
                         <tr>
-                            <td>
-                                {item.startdatestring}
-                               {item.enddatestring}</td>
                             <td>{item.item}</td>
-                            <td>{item.status}</td>
+                            <td>{item.location}</td>
+                            <td><Moment parse="YYYY-MM-DD HH:mm" format="MM / DD / YYYY">{item.startdatestring}</Moment></td>
+                            <td>
+                                <Moment format="hh:mm A" unix tz="America/Salt_Lake_City">{item.startdate}</Moment> - 
+                                <Moment format="hh:mm A" unix tz="America/Salt_Lake_City">{item.enddate}</Moment>
+                            </td>
+                            <td><CheckAvailability status={item.status} /></td>
                         </tr>
                 )
             })}
