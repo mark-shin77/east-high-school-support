5844a04bc38e382dda906cca721c42270b81280d
The Lizard <suzpaylor@gmail.com>
2019-01-28 15:25:45 -0700

    Link to Hours Page

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {Link} from 'react-router-dom';
 import "../assets/css/main.css";
 import "../assets/css/font-awesome.min.css";
 
@@ -18,7 +19,7 @@ class Banner extends Component {
 					<ul className="actions special">
 						<li><div className="button-wrapper"><a href="#one" className="button-wrapper button large primary">Learn More</a></div></li>
 					</ul>
-					<p className="footnote" style={{fontSize: "1.4em", fontWeight: "bold", letterSpacing: "2px"}}> <a href="#">HOURS</a></p>
+					<p className="footnote" style={{fontSize: "1.4em", fontWeight: "bold", letterSpacing: "2px"}}> <Link to="/hours">HOURS</Link></p>
 					
 				</div>
 				<a href="#one" class="more scrolly">Learn more</a>
